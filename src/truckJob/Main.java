d0895dbb4de9885d432f4ba3237c6b07c9b4c01c
OnurKonuk174 <51912619+OnurKonuk174@users.noreply.github.com>
2022-10-24 15:46:53 +0300

    v4tv
    v4t

--- src/truckJob/Main.java
+++ src/truckJob/Main.java
@@ -6,8 +6,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		ArrayList<Driver> drivers= new ArrayList<Driver>();
-		drivers.add(new Driver("Abdulkerimoviç", "Bin Laden", "0373543745"));
-		drivers.add(new Driver("Furkanoviç", "Karacaviç", "38467364786"));
+		drivers.add(new Driver("AbdulkerimoviÃ§", "Bin Laden", "0373543745"));
+		drivers.add(new Driver("FurkanoviÃ§", "KaracaviÃ§", "38467364786"));
 		drivers.add(new Driver("Osman", "Hamza Ahmed", "83 58 43 5687943756"));
 		
 		ArrayList<Truck> trucks=new ArrayList<Truck>();
@@ -18,17 +18,24 @@ public class Main {
 		trucks.get(2).setPlateName("35 ABC 866");
 		trucks.get(2).setBrand("Mercedes");
 		trucks.get(2).setModel("2024");
-		trucks.get(2).setDriver(drivers.get(1));
+		trucks.get(2).setDriver(drivers.get(2));
 		
 		ArrayList<Dispacher> dispachers= new ArrayList<Dispacher>();
-		dispachers.add(new Dispacher("Ahmed", "Dolandýrmaz", "785789415987", "gvbv3t5b"));
-		dispachers.add(new Dispacher("Ýsmet", "Dolandýrabilir", "6386245635", "v3tv3vtv3v"));
-		dispachers.add(new Dispacher("Ahmed", "Dolandýrýr", "645862568", "tvb35v3t5v"));
+		dispachers.add(new Dispacher("Ahmed", "DolandÄ±rmaz", "785789415987", "gvbv3t5b"));
+		dispachers.add(new Dispacher("Ä°smet", "DolandÄ±rabilir", "6386245635", "v3tv3vtv3v"));
+		dispachers.add(new Dispacher("Ahmed", "DolandÄ±rÄ±r", "645862568", "tvb35v3t5v"));
 		
 		ArrayList<Company> companies=new ArrayList<Company>();
-		companies.add(new Company("Karakaçan", "6876485614856", "r4cn4 c4 c4 c4 c 4c 4"));
-		companies.add(new Company("Karakaçmaz", "768746", "dhc 2cr c c re c er"));
+		companies.add(new Company("KarakaÃ§an", "6876485614856", "r4cn4 c4 c4 c4 c 4c 4"));
+		companies.add(new Company("KarakaÃ§maz", "768746", "dhc 2cr c c re c er"));
 		companies.add(new Company("Sina", "64872645823","fceu cr cr  ce rc ec er" ));
+		
+		ArrayList<Job> jobs=new ArrayList<Job>();
+		jobs.add(new Job("Ostim Organize Sanayi/Ankara", "Ä°kitelli Organize Sanayi/Ä°stanbu", 12843.35, 24348.93, 423, companies.get(0), trucks.get(0), dispachers.get(0)));
+		jobs.add(new Job("Ä°zmir", "Samsun", 34578.03, 28123.47, 2147, companies.get(1), trucks.get(1) ,dispachers.get(1) ));
+		jobs.add(new Job("Bolu", "Ankara", 23578.03, 08123.47, 3247, companies.get(2), trucks.get(2) ,dispachers.get(2) ));
+
 	}
 
 }
+
